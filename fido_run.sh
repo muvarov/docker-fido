d5dca9c2cceae7c9d304c70566fe5e29e562932e
Maxim Uvarov <maxim.uvarov@linaro.org>
2022-11-02 18:32:47 +0300

    fido_run.sh add aio
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- fido_run.sh
+++ fido_run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+/usr/lib/fdo/fdo-admin-tool aio --directory /etc/fdo/aio --binary-path /usr/lib/fdo &
 LOG_LEVEL=info /usr/lib/fdo/fdo-manufacturing-server &
 LOG_LEVEL=info /usr/lib/fdo/fdo-owner-onboarding-server &
 LOG_LEVEL=info /usr/lib/fdo/fdo-serviceinfo-api-server &
